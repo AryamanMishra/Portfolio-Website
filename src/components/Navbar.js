9ec96b1cda9e5c6a2a0f311d80f5cc3115b5a6c3
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-05-09 09:03:22 +0530

    updated nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,11 @@ import { Outlet, Link } from "react-router-dom";
 const Navbar = ()=> {
     return (
         <div className='main-nav'>
-            <div className='home-link'>
-                <Link to='/' className='nav-links'><h2>AM</h2></Link>
-            </div>
+            <Link to='/' className='home-link'><h2>AM</h2></Link>
             <div className='routes'>
                 <Link to='/about' className='nav-links'><h2>About</h2></Link>
-                <Link to='/project' className='nav-links'><h2>Projects</h2></Link>
-                <Link to='/about' className='nav-links'><h2>Contact</h2></Link>
+                <Link to='/projects' className='nav-links'><h2>Projects</h2></Link>
+                <Link to='/contact' className='nav-links'><h2>Contact</h2></Link>
             </div>
             <Outlet />
         </div>
