63f1a7ba05ab11771e2b07537ab1b9ac1341e03d
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-07-14 23:03:58 +0530

    updated cards

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Contact.css';
 
-
-const Contact = ()=> {
-    return (
-        <div>
-            Contact
+const Contact = () => {
+  return (
+    <div className="contact-page">
+      <form className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
         </div>
-    )
-}
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
