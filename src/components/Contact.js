e69a2aa65cf9a83205cebc8d55b511c21e322c08
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-08-14 17:27:41 +0530

    updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import '../styles/Contact.css';
+import React from 'react'
+import '../styles/Contact.css'
+import {AiFillStar} from 'react-icons/ai'
 
-const Contact = () => {
-  return (
-    <div className="contact-page">
-      <form className="contact-form">
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" required></textarea>
-        </div>
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
-};
 
-export default Contact;
+const Contact = ()=> {
+	return (
+		<div className='main-contact'>
+			<div className="contact-center">
+				<div className="contact-center-title">
+					<h2>Lets Connect</h2>
+					<AiFillStar 
+						size={32}
+						color='gold'
+						className='star-icon'
+					/>
+				</div>
+				
+				<div className="form-inputs">
+					<input type="text" />
+					<input type="text" />
+					<input type="text" />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Contact
