0d7016703e44b1bd355b81c0ca66fa8171c0bd33
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-08-15 23:12:07 +0530

    updated home page

--- src/components/Home/Experience.js
+++ src/components/Home/Experience.js
@@ -1,37 +1,69 @@
 import React, { useState } from 'react'
-import company_details from '../../data/company_details'
+import experience_details from '../../data/experience_details'
+import {FaArrowLeft,FaArrowRight} from 'react-icons/fa'
+import { MdAdsClick } from 'react-icons/md'
+
 const Experience = ()=> {
 
-    const [activeCompTagIndex,setActiveCompTagIndex] = useState(0)
+    const [expIdx,setExpIdx] = useState(0)
 
-    // const filterList = (comp,idx)=> {
-    //     setActiveCompTagIndex(idx)
-    //     if (idx == 0) {
-    //         let copyList = personal_projects
-    //         setProjectList(copyList.filter((project) => project.tag === 'react'))
-    //     }
-    //     else if (tag === 'rn') {
-    //         let copyList = personal_projects
-    //         setProjectList(copyList.filter((project) => project.tag === 'rn'))
-    //     }
-    //     else if (tag === 'node'){
-    //         let copyList = personal_projects
-    //         setProjectList(copyList.filter((project) => project.tag === 'node'))
-    //     }
-    //     else {
-    //         setProjectList(personal_projects)
-    //     }
-    // }
+    const {id,companyName,title,tenor,desc} = experience_details[expIdx]
 
+    const checkIndex = (idx) => {
+		if (idx < 0)
+			return experience_details.length - 1
+		if (idx >= experience_details.length)
+			return 0
+		return idx
+	}
+
+    const previousExp = ()=> {
+		setExpIdx((index)=> {
+			let newIndex = index - 1
+			return checkIndex(newIndex)
+		})
+	}
+
+	const nextExp = ()=> {
+		setExpIdx((index) => {
+			let newIndex = index + 1
+			return checkIndex(newIndex)
+		})
+	}
     return (
-        
         <div className="main-experience-section">
             <div className="experience-title">
                 <h2>Experience</h2>
                 <div className='underline' style={{width:'11em'}}></div>
             </div>
             <div className="center-experience">
-                
+                <div className="single-experience">
+                    <h2>{companyName}</h2>
+                    <div className="exp-title-div">
+                        <h4>{title}</h4>
+                    </div>
+                    
+                    {/* <p style={{textAlign:'center'}}>{tenor}</p> */}
+                    {
+                        desc.map((d,idx) => {
+                            return (
+                                <div key={idx} className="desc-item">
+                                    <MdAdsClick/>
+                                    <p>{d}</p>
+                                </div>
+                                
+                            )
+                        })
+                    }
+                </div>
+                <div className="exp-button-section">
+                    <button onClick={()=>previousExp()}>
+                        <FaArrowLeft size={30} color='rgb(84, 130, 209)' />
+                    </button>
+                    <button onClick={()=>nextExp()}>
+                        <FaArrowRight size={30} color='rgb(84, 130, 209)'/> 
+                    </button> 
+                </div>
             </div>
         </div>
     )
---
+++ src/data/experience_details.js
@@ -0,0 +1,27 @@
+const experience_details = [
+    {
+        "id":1,
+        "companyName" : "Quantiphi Analytics",
+        "title" : "Data Engineering Intern",
+        "tenor": "March 2023 - July 2023",
+        "desc" : [
+            "Created real-time ETL data pipelines using GCP (Google Cloud Platform)",
+            "Automated the process of uploading tables to BigQuery from cloud storage using Cloud Function triggers.",
+            "Used BigQuery to process and query obtained data according to business needs."
+        ]
+    },
+    {
+        "id":2,
+        "companyName" : "Persistent Systems Limited",
+        "title" : "Summer Intern",
+        "tenor" : "June 2022 - August 2022",
+        "desc" : [
+            "Developed a Serverless image uploader Web application on AWS, supporting 1000 monthly users.",
+            "Utilized AWS services such as AWS Lambda, API Gateway, AWS Cognito, and Amazon S3",
+            "The application is designed to support up to 5000 API calls and. Users can upload own images and see other’s images.",
+            "Estimated monthly cost for running the application is $66.910, based on AWS cost explorer."
+        ]
+    }     
+]
+
+export default experience_details
